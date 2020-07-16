4df1e9c6ee8b07b9334e929a2ecd6ed65f30d157
J.Gabriel <gabrieljaime656@gmail.com>
2020-07-16 09:14:26 -0300

    projetozerado

---
+++ demo/src/test/java/com/revassunto/demo/RevAssuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revassunto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevAssuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
